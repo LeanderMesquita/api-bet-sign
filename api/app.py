aadca2edbcb567c191f4fd0d64bddf414a6207dd
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-02 00:35:43 -0300

    FEAT: initial app routes structure

---
+++ api/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request,jsonify
+from werkzeug.utils import secure_filename
+from api.index import start_injection
+from api.src.utils.functions.read_dataframe import read_dataframe
+from api.src.utils.logger.index import log
+
+app = Flask(__name__)
+
+
+@app.route('/unique-register', methods=['POST'])
+def unique_registering():
+    return None
+
+@app.route('/upload-file', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    if file:
+        filename = secure_filename(file.filename)
+        log.info(f'Recieved file: {filename}')
+        df = read_dataframe(file)
+
+        start_injection(df, 'multiple_injection')
+        return jsonify({'message': 'File successfully uploaded and processed'}), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
