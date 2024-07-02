f2ae9351621895ada5df5ab686a3237ad32d6461
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-02 00:01:50 -0300

    FEAT: implementing factory method

---
+++ api/factory/index.py
@@ -0,0 +1,10 @@
+from api.src.tasks.multiple_inject import MultipleInjection
+from api.src.tasks.multiple_inject import SingleInjection
+
+class TaskFactory:
+    @staticmethod
+    def create_task(task_type, row):
+        if task_type == 'multiple_injection':
+            return MultipleInjection(row)
+        elif task_type == 'single_injection':
+            return SingleInjection(row)
