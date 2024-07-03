8f18ed7c8c2637fef3506a82bb2949e1dca1945b
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-02 21:24:10 -0300

    FEAT: initiate browser function

--- api/index.py
+++ api/index.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
+import os 
 from api.factory.index import TaskFactory
 from api.src.utils.functions.error_report import error_report
 from api.src.utils.functions.successfully_report import successfully_report
 from api.src.utils.logger.index import log
+from playwright.sync_api import Page, expect
+from playwright.sync_api import sync_playwright
+
 
 def start_injection(df, selected_task:str):
     try:
@@ -11,7 +16,7 @@ def start_injection(df, selected_task:str):
             try:
                 log.info(f'Registering account ({row['Nome']}).')
                 task = TaskFactory.create_task(selected_task)
-                task.execute()
+                task.execute(page=construct_browser(server='', username='odFzSl36zp', password='87478941'))
 
                 log.success(f'Account ({row['Nome']}) was registered successfully!')
                 successfully_report(row['CPF'], row['Nome'])
@@ -23,3 +28,21 @@ def start_injection(df, selected_task:str):
 
     except Exception as e:
         log.critical(f'An critical error ocurred!: {e}') 
+
+
+def construct_browser(server:str, username:str, password:str, is_headless:bool = False) -> Page:
+    
+    load_dotenv()
+    
+    proxy_options = {
+        "server": server,
+        "username": username,
+        "password": password
+    }
+    
+    with sync_playwright() as p:
+        browser = p.chromium.launch(proxy={"server": "per-context"}, headless=is_headless)
+        context = browser.new_context(proxy=proxy_options)
+        page = context.new_page()
+        page.goto(os.getenv('BASE_URL'))
+        page.get_by_role("button", name="Aceitar todos os cookies").click()
