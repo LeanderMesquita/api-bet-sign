9e98cc649a6e60b2c51f7fe9b01ed5472dd304d3
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-03 03:38:23 -0300

    WIP: trying to connect proxy and constructing browser

--- api/index.py
+++ api/index.py
@@ -16,7 +16,7 @@ def start_injection(df, selected_task:str):
             try:
                 log.info(f'Registering account ({row['Nome']}).')
                 task = TaskFactory.create_task(selected_task)
-                task.execute(page=construct_browser(server='', username='odFzSl36zp', password='87478941'))
+                task.execute(page=construct_browser(server='177.37.251.155', username='odFzSl36zp', password='87478941'))
 
                 log.success(f'Account ({row['Nome']}) was registered successfully!')
                 successfully_report(row['CPF'], row['Nome'])
@@ -31,18 +31,19 @@ def start_injection(df, selected_task:str):
 
 
 def construct_browser(server:str, username:str, password:str, is_headless:bool = False) -> Page:
-    
-    load_dotenv()
-    
-    proxy_options = {
-        "server": server,
-        "username": username,
-        "password": password
-    }
-    
-    with sync_playwright() as p:
-        browser = p.chromium.launch(proxy={"server": "per-context"}, headless=is_headless)
-        context = browser.new_context(proxy=proxy_options)
-        page = context.new_page()
-        page.goto(os.getenv('BASE_URL'))
-        page.get_by_role("button", name="Aceitar todos os cookies").click()
+    try:
+        load_dotenv()
+        proxy_options = {
+            "server": server,
+            "username": username,
+            "password": password
+        }
+        
+        with sync_playwright() as p:
+            browser = p.chromium.launch(proxy={"server": "per-context"}, headless=is_headless)
+            context = browser.new_context(proxy=proxy_options)
+            page = context.new_page()
+            page.goto(os.getenv('BASE_URL'))
+            page.get_by_role("button", name="Aceitar todos os cookies").click()
+    except Exception as e:
+        log.error(f'Error in browser construction: {e}')
