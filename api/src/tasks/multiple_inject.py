3c449584e87a9f631c543ccccd3ec3a9859ef196
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-02 21:25:25 -0300

    FEAT: multiple injection new declaration

--- api/src/tasks/multiple_inject.py
+++ api/src/tasks/multiple_inject.py
@@ -1,7 +1,8 @@
 import re
 from playwright.sync_api import Page, expect
 from time import sleep
-
+from dotenv import load_dotenv
+import os
 from api.src.tasks.base_task import BaseTask
 from api.src.utils.functions.click_and_fill import click_and_fill
 from api.src.utils.functions.split_date import split_date
@@ -12,12 +13,9 @@ class MultipleInjection(BaseTask):
         self.row = row
 
     def execute(self, page: Page) -> None:
-        ## going to superbet register page
-        page.goto("https://superbet.com/pt-br/register?utm_source=google&utm_medium=ppc&utm_campaign=ppc-bet-acq-ext-sitelink_onboard-brand-all-sup&gad_source=1&gclid=CjwKCAjwp4m0BhBAEiwAsdc4aIvP-ORBTZXG7hDmPVNE6OS3i3lVBSnfOx9V-RbIy8OUS6Z2DcDj1RoCT3kQAvD_BwE")
 
         ## first step
-        page.get_by_role("button", name="Aceitar todos os cookies").click()
-
+        
         day, month, year = split_date(self.row['Data de Nascimento'])
         page.get_by_placeholder("dd").click()
         page.get_by_placeholder("dd").fill(day)
