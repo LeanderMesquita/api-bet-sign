689ab5cb75ffcd238518acd6a6ede7272946942b
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-04 00:11:25 -0300

    FEAT: config class

---
+++ api/configs/index.py
@@ -0,0 +1,33 @@
+from playwright.sync_api import sync_playwright
+from api.src.utils.logger.index import log
+from dotenv import load_dotenv
+import os 
+
+class Configure:
+    @staticmethod
+    def construct_browser(self, server: str, username: str = None, password: str = None, is_headless: bool = False):
+    
+        try:
+            
+            proxy_options = {
+                "server": server,
+                "username": username,
+                "password": password
+            }
+
+            p = sync_playwright().start()
+
+            log.debug('Launching browser with proxy settings')
+            browser = p.chromium.launch(headless=is_headless, proxy={"server": "per-context"})
+            context = browser.new_context(proxy=proxy_options)
+            page = context.new_page()
+            
+            load_dotenv()
+            base_url = os.getenv('BASE_URL')
+            log.debug(f"Filling URL: {base_url}")
+            page.goto(base_url, timeout=60000)
+
+            return page, p
+        
+        except Exception as e:
+            log.error(f'Error in browser construction: {e}')
