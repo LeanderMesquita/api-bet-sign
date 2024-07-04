c66954a019905d1c50cc8e0cf285e204b3695bb3
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-03 21:30:10 -0300

    REFACTOR: adjusting split date logic to recieve format date

--- api/src/utils/functions/split_date.py
+++ api/src/utils/functions/split_date.py
@@ -1,7 +1,11 @@
+from api.src.utils.functions.format_date import format_date
+
 def split_date(raw_date: str):
-    if '/' not in raw_date:
+    formatted_date = format_date(raw_date)
+
+    if '/' not in formatted_date:
         raise ValueError('Invalid format. Date must be: "DD/MM/YYYY"') 
         
-    day, month, year = raw_date.split('/')
+    day, month, year = formatted_date.split('/')
     return day, month, year
 
