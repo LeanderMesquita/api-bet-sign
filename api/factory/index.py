753fdb185c9c08024595aac78cef8cdfc054af61
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-04 01:23:47 -0300

    FEAT: new tasks building

--- api/factory/index.py
+++ api/factory/index.py
@@ -1,10 +1,16 @@
 from api.src.tasks.multiple_inject import MultipleInjection
 from api.src.tasks.single_injection import SingleInjection
 
+from api.src.tasks.dataframe_injection import DataframeInjection
+from api.src.tasks.json_injection import JSONInjection
 class TaskFactory:
     @staticmethod
-    def create_task(task_type, row, page):
+    def create_task(task_type, data, page):
         if task_type == 'multiple_injection':
-            return MultipleInjection(row, page)
+            return MultipleInjection(data, page)
         elif task_type == 'single_injection':
-            return SingleInjection(row, page)
+            return SingleInjection(data, page)
+        elif task_type == 'dataframe_injection':
+            return DataframeInjection(data, page)
+        elif task_type == 'json_injection': 
+            return JSONInjection(data, page)
