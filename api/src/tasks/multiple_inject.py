a4926a26939300bcc7c3a5440ce85f80272d0b65
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-03 22:54:46 -0300

    FEAT: multiple injection structure

--- api/src/tasks/multiple_inject.py
+++ api/src/tasks/multiple_inject.py
@@ -3,41 +3,57 @@ from playwright.sync_api import Page, expect
 from time import sleep
 from dotenv import load_dotenv
 import os
+from api.src.utils.logger.index import log
 from api.src.tasks.base_task import BaseTask
 from api.src.utils.functions.click_and_fill import click_and_fill
 from api.src.utils.functions.split_date import split_date
 
 
 class MultipleInjection(BaseTask):
-    def __init__(self, row):
+    def __init__(self, row, page):
         self.row = row
+        self.page = page
 
-    def execute(self, page: Page) -> None:
+    def execute(self) -> None:
+        
+        
+        log.debug('Accepting cookies')
+        self.page.get_by_role("button", name="Aceitar todos os cookies").click()
 
         ## first step
         
-        day, month, year = split_date(self.row['Data de Nascimento'])
-        page.get_by_placeholder("dd").click()
-        page.get_by_placeholder("dd").fill(day)
-        page.get_by_placeholder("mm").fill(month)
-        page.get_by_placeholder("yyyy").fill(year)
+        day, month, year = split_date(self.row['Nascimento'])#self.row['Nascimento']
+        self.page.get_by_placeholder("dd").click()
+        sleep(1)
+        log.debug(f'Filling selector: "dd" with value: {day}')
+        self.page.get_by_placeholder("dd").fill("28")#day
+        sleep(1)
+        log.debug(f'Filling selector: "mm" with value: {month}')
+        self.page.get_by_placeholder("mm").fill("09")#month
+        sleep(1)
+        log.debug(f'Filling selector: "yyyy" with value: {year}')
+        self.page.get_by_placeholder("yyyy").fill("1963")#year
 
-        click_and_fill(page, selector="CPF", value="229.126.063-49_", press="Enter")#self.row['CPF']
+        click_and_fill(self.page, selector="CPF", value="229.126.063-49", press="Enter")#self.row['CPF']
         
         #second step
-        click_and_fill(page, selector="endereço", value="Rua teste")#self.row['Endereço']
+        click_and_fill(self.page, selector="endereço", value="Rua teste")#self.row['Endereço']
     
-        page.get_by_label("cidade", exact=True).click()
-        page.get_by_label("cidade", exact=True).fill("fortaleza")#self.row['Cidade']
+        
+        self.page.get_by_label("cidade", exact=True).click()
+        sleep(1)
+        log.debug(f'Filling selector: "cidade" with value: {"?CIDADE"}')
+        self.page.get_by_label("cidade", exact=True).fill("fortaleza")#self.row['Cidade']
 
-        click_and_fill(page, selector="cep", value="60000-600")#self.row['CEP']
-        click_and_fill(page, selector="Número de telefone", value="940028922", press="Enter")#self.row['Telefone']
+        click_and_fill(self.page, selector="cep", value="60000-600")#self.row['CEP']
+        click_and_fill(self.page, selector="Número de telefone", value="940028922", press="Enter")#self.row['Telefone']
 
         #third step
-        click_and_fill(page, selector="E-mail", value="emailteste@gmail.com")#self.row['Email']
-        click_and_fill(page, selector="nome de usuário", value="testebet2")#self.row['Nome Usuario]
-        click_and_fill(page, selector="senha", value="davi_chefinho_aumenta_meu_salario")#self.row['Senha']
+        click_and_fill(self.page, selector="E-mail", value="emailteste@gmail.com")#self.row['Email']
+        click_and_fill(self.page, selector="nome de usuário", value="testebet2")#self.row['Nome Usuario]
+        click_and_fill(self.page, selector="senha", value="Edra36Edra")#self.row['Senha']
 
-        page.locator("label").filter(has_text="Tenho 18 anos ou mais de").click()
-        page.get_by_label("Tenho 18 anos ou mais de").press("Enter")
+        self.page.locator("label").filter(has_text="Tenho 18 anos ou mais de").click()
+        sleep(1)
+        self.page.get_by_label("Tenho 18 anos ou mais de").press("Enter")
         
