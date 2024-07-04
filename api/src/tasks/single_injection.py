319e4682feee213c65cee7b44b7ec5ba32f8f3b5
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-07-04 01:26:48 -0300

    DELETE: older single inject

--- api/src/tasks/single_injection.py
+++
@@ -1,8 +0,0 @@
-from api.src.tasks.base_task import BaseTask
-
-class SingleInjection(BaseTask):
-    def __init__(self, row):
-        self.row = row
-
-    def execute(self):
-        return super().execute()
